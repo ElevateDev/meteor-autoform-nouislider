21febade12d24db0eddc8fd2ea4e1f7550782a0a
Jimmie Butler <jimmiebtlr@gmail.com>
2015-04-06 01:21:06 -0500

    Add single value mode.
    Bug fixes for multi value mode as well.

--- autoform-nouislider.js
+++ autoform-nouislider.js
@@ -3,54 +3,69 @@
 AutoForm.addInputType("noUiSlider", {
   template: "afNoUiSlider",
   valueOut: function(){
-    var first = parseInt($(this).val()[0]);
-    var second = parseInt($(this).val()[1]);
-    var value = {
-      lower: first > second ? second : first,
-      upper: first > second ? first : second
-    };
-    return value;
+    var elem = $(this[0]);
+    var slider = $(elem.find('.nouislider')[0]);
+    if( this.attr("data-type") === "Object" ){
+      var first = parseInt(slider.val()[0]);
+      var second = parseInt(slider.val()[1]);
+      var value = {
+        lower: first > second ? second : first,
+        upper: first > second ? first : second
+      };
+      return value;
+    }else{
+      return slider.val();
+    }
   }
 });
 
 Template.afNoUiSlider.helpers({
   atts: function () {
-    var template = Template.instance();
-    return _.omit(_.omit(template.atts, 'noUiSliderOptions'),'noUiSlider_pipsOptions'); 
+    var data = Template.instance().data;
+    var atts = data.atts;
+    atts["data-type"] = data.schemaType.name;
+    return _.omit(_.omit(atts, 'noUiSliderOptions'),'noUiSlider_pipsOptions'); 
   }
 });
 
 Template.afNoUiSlider.rendered = function () {
   var template = this;
-  var $s = template.$('.form-control');
-
-  var start;
-  if( this.data.value && this.data.value.lower ){
-    start = [
-      this.data.value.lower,
-      this.data.value.upper
-    ];
-  }else{
-    start = [
-      this.data.min ? this.data.min : 0, 
-      this.data.max ? this.data.max : 100
-    ];
+  var $s = template.$('.nouislider');
+  var options = {};
+  if( template.data.atts.noUiSliderOptions ){
+    options = template.data.atts.noUiSliderOptions;
   }
+
+  // Adjust data initalization based on schema type
+  var start;
+  if( this.data.schemaType.name === "Object" ){
+    if( this.data.value && this.data.value.lower ){
+      start = [
+        this.data.value.lower,
+        this.data.value.upper
+      ];
+    }else{
+      start = [
+        this.data.min ? this.data.min : 0, 
+        this.data.max ? this.data.max : 100
+      ];
+    }
+    options.connect = true;
+  }else{
+    if( this.data.value ){
+      start = this.data.value;
+    }else{
+      start = 0;
+    }
+  }
+  options.start = start;
 
   var range = {
     min: this.data.min ? this.data.min : 0, 
     max: this.data.max ? this.data.max : 100
   };
 
-  var options = {};
-  if( template.data.atts.noUiSliderOptions ){
-    options = template.data.atts.noUiSliderOptions;
-  }
-  options = _.extend({
-    start: start,
-    range: range,
-    connect: true
-  },options);
+  options.range = range;
 
   // default step to 1 if not otherwise defined
   if( !options.step ){
@@ -58,14 +73,14 @@ Template.afNoUiSlider.rendered = function () {
   }
 
   $s.noUiSlider(options || {});
-  template.$('.form-control').on({
+  /*template.$('.form-control').on({
     slide: function(){
-      $(template.$('.form-control')).change();
+      template.$('.form-control').change();
     }
-  });
+  });*/
   
   if( template.data.atts.noUiSlider_pipsOptions ){
-    template.$('.form-control').noUiSlider_pips(
+    $s.noUiSlider_pips(
       template.data.atts.noUiSlider_pipsOptions
     );
   }
