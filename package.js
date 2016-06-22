1c4ae335d4d4d83755046be9efcbeedbcabead17
Raiyan Kamal <raiyan.kamal@gmail.com>
2016-06-22 00:12:38 -0400

    Version bumped to 0.2.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'elevatedevdesign:autoform-nouislider',
   summary: 'Dual value slider for autoform.',
-  version: '0.1.1',
+  version: '0.2.0',
   git: 'https://github.com/ElevateDevelopmentAndDesign/meteor-autoform-nouislider'
 });
 
