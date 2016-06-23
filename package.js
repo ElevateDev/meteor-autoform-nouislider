1612e6e820db64c7714e790558b0168a87a5557c
Raiyan Kamal <raiyan.kamal@gmail.com>
2016-06-23 00:03:15 -0400

    Removed unused dependency aldeed:template-extension

--- package.js
+++ package.js
@@ -10,7 +10,6 @@ Package.onUse(function(api) {
 
   api.use('templating@1.0.0');
   api.use('blaze@2.0.0');
-  api.use('aldeed:template-extension@4.0.0');
   api.use('rcy:nouislider@7.0.7_2');
   api.use('aldeed:autoform@4.0.0 || 5.0.0');
   api.addFiles([
