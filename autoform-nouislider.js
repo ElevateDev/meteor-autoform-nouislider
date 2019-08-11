37d814fa93e693be96ea55566caa738ccd571671
Jan Küster <jkuester@uni-bremen.de>
2019-08-11 11:26:38 +0200

    update compatibility with newer autoform / simpl-schema

--- autoform-nouislider.js
+++ autoform-nouislider.js
@@ -1,27 +1,25 @@
 /* global AutoForm, _, Template */
 
 import noUiSlider from 'nouislider'
-import 'nouislider/src/nouislider.css'
-import 'nouislider/src/nouislider.pips.css'
-import 'nouislider/src/nouislider.tooltips.css'
+import 'nouislider/distribute/nouislider.css'
 
 AutoForm.addInputType('noUiSlider', {
   template: 'afNoUiSlider',
   valueOut: function () {
     const slider = this.find('.nouislider')[ 0 ]
-    const isDecimal = this.closest('.at-nouislider').data('decimal')
+    const sliderData = slider.noUiSlider.get()
 
     if (this.attr('data-type') === 'Object') {
+      const isDecimal = this.closest('.at-nouislider').data('decimal')
       const parser = (isDecimal) ? parseFloat : parseInt
-      const first = parser.call(null, slider.noUiSlider.get()[ 0 ])
-      const second = parser.call(null, slider.noUiSlider.get()[ 1 ])
-      const value = {
+      const first = parser.call(null, sliderData[ 0 ])
+      const second = parser.call(null, sliderData[ 1 ])
+      return {
         lower: first > second ? second : first,
         upper: first > second ? first : second
       }
-      return value
     } else {
-      return slider.noUiSlider.get()
+      return sliderData
     }
   }
 })
@@ -30,7 +28,7 @@ Template.afNoUiSlider.helpers({
   atts: function () {
     const data = Template.currentData() // get data reactively
     const atts = data.atts
-    atts[ 'data-type' ] = data.schemaType.name
+    atts[ 'data-type' ] = data.schemaType.name || 'Object' // fallback if type is a Schema and name becomes undefined
     if (atts[ 'class' ]) {
       atts[ 'class' ] += ' at-nouislider'
     } else {
@@ -38,7 +36,6 @@ Template.afNoUiSlider.helpers({
     }
 
     atts.doLabels = (atts.labelLeft || atts.labelRight)
-
     atts[ 'data-decimal' ] = data.decimal
 
     return _.omit(atts, 'noUiSliderOptions', 'noUiSlider_pipsOptions')
